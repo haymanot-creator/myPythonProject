02d2ee1e491d732217f4150f8b492fd4fa4665a0
hayma <haymanot2704@gmail.com>
2026-01-28 11:45:20 +0200

    first commit

--- training_pyton/test_class_special_cases.py
+++ training_pyton/test_class_special_cases.py
@@ -1,9 +1,18 @@
-class TestClassesExample():
+class Test Swaglabs Example():
 
     def test_swaglabs_correct_user_login_with_class(self, setup_playwright):
         print("into test_class_correct_login")
         page = setup_playwright
         page.goto("https://www.saucedemo.com/")
         elements_with_spaces = page.query_selector_all(".input_error.form_input")  # example how to handle spaces in class
+
+        elements_partial = page.query_selector_all("[class*=input_error]")
+
+    def test_swaglabs_correct_password_login(self, setup_playright):
+        print("into test_swag_labs_correct_password_login")
+        page = setup_playright
+        page.goto("https://www.saucedemo.com/")
+        elements_with_spaces = page.query_selector_all("[class*=input_error]")
+
 
         elements_partial = page.query_selector_all("[class*=input_error]")
--- training_pyton/test_swaglabs.py
+++ training_pyton/test_swaglabs.py
@@ -1,33 +1,20 @@
 
 
-class TestSwagLabs(object):
+class TestSwaglabs :
 
-    def test_swaglabs_correct_login(self , setup_playwright ):
+    def test_swaglabs_correct_login(self ,setup_playwright):
         print("into test_swaglabs_correct_login")
-        page = setup_playwright()
-        page.goto("https://www.saucedemo.com")
+        page = setup_playwright
+        page.goto("https://www.saucedemo.com/")
+
+
         user = page.locator("[id='user-name']")
         user.fill('standard_user')
         password = page.locator("[id='password']")
         password.type("secret_sauce")
-        login_button = page.locator("[name='login_button']")
-        login_button.click()
-        print(f"current url is{page.url}")
+        login_btn =page.get_by_text("login")
+        login_btn.click()
         current_url = page.url
         assert current_url == "https://www.saucedemo.com/inventory.html","current url is not expected"
         print("test end")
 
-    def test_swag_labs_incorrect_password_logout(self,setup_playwright):
-        print("into test_swag_labs_incorrect_password_logout")
-        page = setup_playwright
-        page.goto("https://www.saucedemo.com")
-        user = page.locator("[id='user-name']")
-        user.fill('standard_user')
-        password = page.locator("[id='password']")
-        password.type("fail")
-        login_button = page.locator("[name='login_button']")
-        login_button.click()
-        print(f"current url is{page.url}")
-        current_url = page.url
-        assert current_url == "https://www.saucedemo.com ,current url is not expected"
-        print("test end")
